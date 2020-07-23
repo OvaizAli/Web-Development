2b1271e2febf412c23e33ca1bc6cac68f534e122
OvaizAli <ovaizali123@gmail.com>
2020-07-24 00:04:01 +0500

    Final Version

--- Saylani Mass IT Training/Assignments/Quiz Application/questions.js
+++ Saylani Mass IT Training/Assignments/Quiz Application/questions.js
@@ -1,12 +1,52 @@
 const quiz = [
     {
-        q:"Which month comes right before june ?",
-        options:['May','September','July','August'],
+        q:"Which is the national bird of Pakistan?",
+        options:['Chakor','Eagle','Sparrow','Parrot'],
         answer: 0
     },
     {
-        q:"Which is NOT the primary color ?",
-        options:['Yellow','Blue','Green','Red'],
+        q:"Which Pakistani Prime Minister received Indian Prime Minister Atal Behari Vajpayee when he arrived by bus to Lahore?",
+        options:['Nawaz Shariff','Benazir Bhutto','Yousuf Raza Gilani','Imran Khan'],
         answer: 0
+    },
+    {
+        q:"Where was General Pervez Musharraf born?",
+        options:['Delhi','Karachi','Bombay','Hyderabad'],
+        answer: 0
+    },
+    {
+        q:"What is the meaning of Pakistan?",
+        options:['Holy Land','Peaceful Land','Rebublic','Prosperous Land'],
+        answer: 0
+    },
+    {
+        q:"Which is the national flower of Pakistan?",
+        options:['Jasmine','Rose','Sun Flower','Tulip'],
+        answer: 0
+    },
+    {
+        q:"Who designed Pakistan’s national flag?",
+        options:['Ameer-ud-din Khidwai','Benazir Bhutto','M.A Jinnah','Chaudry Brothers'],
+        answer: 0
+    },
+    {
+        q:"Who is the current foreign minister of Pakistan?",
+        options:['Nawaz Shariff','Bilawal Bhutto Zardari','Yousuf Raza Gilani','Shah Mahmood Qureshi'],
+        answer: 3
+    },
+    {
+        q:"How is Pakistan’s film industry known?",
+        options:['Lollywood','Tollywood','Bollywood','Pakiwood'],
+        answer: 0
+    },
+    {
+        q:"Which is the national animal of Pakistan?",
+        options:['Markhor','Rabbit','Cow','Snow Leopard'],
+        answer: 0
+    },
+    {
+        q:"Who is the Captain of Pakistan tam in Tes Cricket?",
+        options:['Babar Azam','Sarfaraz Ahmed','Azhar Ali','Imran Khan Jr'],
+        answer: 2
     }
 ]
